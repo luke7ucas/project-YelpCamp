215208a9f7607fe2dd6c00703dfe6b98976d770a
LukaszL <luke7ucas@gmail.com>
2021-08-29 00:09:21 +0100

    changes to seeds in order to seed Mongo Atlas

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ app.use((err, req, res, next) => {
 })
 /// ---
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;  // when we push to Heroku there will be env.PORT variable available
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 });
--- seeds/index.js
+++ seeds/index.js
@@ -5,7 +5,9 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -22,14 +24,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
+//    await Campground.deleteMany({});
     console.log('from: seeds/index.js: Deleted all campgrounds.');
     for (let i = 0; i < 150; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             // YOUR USER ID
-            author: '612a9ca52e0f944ed824641d',
+            author: '612aa666bee35e545c567444',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
